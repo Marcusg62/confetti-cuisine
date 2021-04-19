e2a14615eb41a44d7f890c7f2d2a20f0dc7b8ed5
Marcus Gallegos <marcus.gallegos62@gmail.com>
2021-04-18 18:01:03 -0600

    completed api classwork

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,25 +1,3 @@
-var courses = [
-    {
-        title: "Rasberry Cake",
-        cost: 50
-    },
-    {
-        title: "Soup",
-        cost: 50
-    },
-    {
-        title: "Chips",
-        cost: 50
-    },
-    {
-        title: "Bread",
-        cost: 50
-    },
-    {
-        title: "Donuts",
-        cost: 50
-    },
-]
 module.exports = {
     index: (req, res) => {
         res.render('index')
